2f2810156f0cec0cf04e4c3c3b18bc7bb92d58d4
jsouza11 <jake_souza@csu.fullerton.edu>
2025-05-13 21:05:51 -0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { LoadingScreen } from './components/LoadingScreen'
 
 function App() {
+  return (
+    <>
+        <LoadingScreen/>
+        </>
+
+
+  )
+    
+    
+        
   
 }
 
