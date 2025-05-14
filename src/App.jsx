1f76429a480e4144c615f82d482cc01e97ca959a
jsouza11 <143134566+jsouza11@users.noreply.github.com>
2025-05-13 23:36:02 -0700

    Nav bar and mobile bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import './App.css'
 import { LoadingScreen } from './components/LoadingScreen'
+import "./index.css"
+import { useState } from 'react';
+import { Navbar } from './components/Navbar'
+import { MobileMenu } from './components/MobileMenu'
+import { useEffect } from 'react';
+
+
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+
+
   return (
     <>
-        <LoadingScreen/>
-        </>
+        {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />} {" "}
+        <div className={`min-h-screen transition-opacity duration-700 ${ isLoaded ? 'opacity-100' : 'opacity-0'} 
+        bg-black text-gray-100`}
+        >
+
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        </div>
+
+    </>
 
 
   )
