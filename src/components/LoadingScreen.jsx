1dead0dc182c57956844f46114382aaf46e993d1
jsouza11 <143134566+jsouza11@users.noreply.github.com>
2025-05-13 20:07:48 -0700

    init
    intital

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,12 @@
+export const LoadingScreen = () => {
+
+
+    return (
+    <div className="fixes inset -0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
+        <div className="mb-4 text-4xl font-mono font-bold">Hello World <span className="animate-blink ml-1"> | </span>
+        </div>
+
+
+    </div>>
+    );
+};
